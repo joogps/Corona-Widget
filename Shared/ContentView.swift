feafb0fe3dd926be5376850ff412e1084831afab
joogps <joaogabrielps13@hotmail.com>
2020-06-23 18:28:08 -0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by João Gabriel Pozzobon dos Santos on 23/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/CoronaWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoronaWidgetApp.swift
+//  Shared
+//
+//  Created by João Gabriel Pozzobon dos Santos on 23/06/20.
+//
+
+import SwiftUI
+
+@main
+struct CoronaWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
